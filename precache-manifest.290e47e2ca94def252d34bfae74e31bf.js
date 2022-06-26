0ef6f9e5e8830037346cd0692f41d2378c10f1cf
Junaid <zulfiqar.junaid95@gmail.com>
2022-06-25 21:20:48 -0700

    Updates

---
+++ precache-manifest.290e47e2ca94def252d34bfae74e31bf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ee5c4a114b530c2065ea281ab45423",
+    "url": "/Personal-Portfolio-Junaid95/index.html"
+  },
+  {
+    "revision": "732a09434f1cc0ec773d",
+    "url": "/Personal-Portfolio-Junaid95/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "c586edabcb8cbf516275",
+    "url": "/Personal-Portfolio-Junaid95/static/css/main.b7965308.chunk.css"
+  },
+  {
+    "revision": "732a09434f1cc0ec773d",
+    "url": "/Personal-Portfolio-Junaid95/static/js/2.e22f54be.chunk.js"
+  },
+  {
+    "revision": "0db18f2f56e579bdbf0e639ec3e0b99a",
+    "url": "/Personal-Portfolio-Junaid95/static/js/2.e22f54be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c586edabcb8cbf516275",
+    "url": "/Personal-Portfolio-Junaid95/static/js/main.66e7cd0b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Personal-Portfolio-Junaid95/static/js/main.66e7cd0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "903fae0560d05b324308",
+    "url": "/Personal-Portfolio-Junaid95/static/js/runtime-main.ed340acc.js"
+  },
+  {
+    "revision": "2e130bf9f8aeeb51e8958edcceddb393",
+    "url": "/Personal-Portfolio-Junaid95/static/media/color.2e130bf9.PNG"
+  },
+  {
+    "revision": "360168d3f0d51ed8f6c5b81cc55267e2",
+    "url": "/Personal-Portfolio-Junaid95/static/media/covid.360168d3.png"
+  },
+  {
+    "revision": "20e45d9afe66e0e6533dcd44fb9a7846",
+    "url": "/Personal-Portfolio-Junaid95/static/media/dev.20e45d9a.svg"
+  },
+  {
+    "revision": "b8492dc9281227857ed3d8e0224b7f63",
+    "url": "/Personal-Portfolio-Junaid95/static/media/fyp.b8492dc9.png"
+  },
+  {
+    "revision": "c6530e20b3d220d3979f4515bbdd54e8",
+    "url": "/Personal-Portfolio-Junaid95/static/media/groc_bud.c6530e20.PNG"
+  },
+  {
+    "revision": "66747770a0facee24ae111f6837a50f0",
+    "url": "/Personal-Portfolio-Junaid95/static/media/netflix.66747770.PNG"
+  },
+  {
+    "revision": "e324d3000236bc43b95fd0d1d88ec37f",
+    "url": "/Personal-Portfolio-Junaid95/static/media/shake.e324d300.svg"
+  },
+  {
+    "revision": "7765d245a19c8175c4ca53d9f022c8e5",
+    "url": "/Personal-Portfolio-Junaid95/static/media/stripe.7765d245.PNG"
+  }
+]);
